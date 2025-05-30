ee8c5768140a342f58ec71c6d2faebef5fdcc049
Kento Yamamoto <key7135g@gmail.com>
2025-05-30 21:42:57 +0900

    add: トップページのルーティングとviewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root 'home#index'
+
   resources :users do
     member do
       get 'calendar'
     end
   end
+
 end
